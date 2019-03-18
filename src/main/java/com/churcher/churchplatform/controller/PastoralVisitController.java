45c12363b7e4653330bd2d1d9764635cd205bab6
Dawid Cielicki <gamer@o2.pl>
2019-03-18 16:31:09 +0100

    JSON.

--- src/main/java/com/churcher/churchplatform/controller/PastoralVisitController.java
+++ src/main/java/com/churcher/churchplatform/controller/PastoralVisitController.java
@@ -1,13 +1,21 @@
 package com.churcher.churchplatform.controller;
 
-import com.churcher.churchplatform.model.PastoralVisit;
 import com.churcher.churchplatform.service.PastoralVisitService;
+import com.sun.javafx.collections.MappingChange;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.RequestWrapper;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 public class PastoralVisitController {
@@ -19,14 +27,42 @@ public class PastoralVisitController {
         this.pastoralVisitService = pastoralVisitService;
     }
 
-//    @GetMapping("/setPastoralVisit")
-//    public String pastoralVisitEditPage(){
-//
+    @GetMapping("/setPastoralVisit")
+    public String pastoralVisitEditPage(){
+
 //        PastoralVisit pastoralVisit =
-//
-//        return "setPastoralVisit";
-//    }
-//
+        return "setPastoralVisit";
+    }
+
+    @GetMapping("/json")
+    public String jsonTest(){
+        return "test";
+    }
+
+    @PostMapping("/json2")
+    public String addNewWorker(@RequestParam(name = "pastoralVisit") String name, Model model) {
+        Map<LocalDate, Set<String>> pastoral = new HashMap<>();
+        try {
+            JSONObject object = new JSONObject(name);
+            JSONArray array = object.getJSONArray("dates");
+            for(int i = 0; i < array.length(); i++){
+                JSONObject jsob = array.getJSONObject(i);
+                String stringDate = jsob.get("date").toString();
+                LocalDate date = LocalDate.of(Integer.valueOf(stringDate.substring(0,4)), Integer.valueOf(stringDate.substring(5,7)), Integer.valueOf(stringDate.substring(8,10)));
+                JSONArray ar = jsob.getJSONArray("streets");
+                for(int j = 0; j < ar.length(); j++){
+                    Set<String> set = new HashSet<>();
+                    set
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return "json2";
+    }
+
+
 //    @PostMapping("/savePastoralVisit")
 //    public String savePastoralVisit{
 //        pastoralVisitService.save();
--- src/main/java/com/churcher/churchplatform/model/Test.java
+++ src/main/java/com/churcher/churchplatform/model/Test.java
@@ -1,26 +1,25 @@
 package com.churcher.churchplatform.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Test {
 
-    private static final List<LocalDate> calendar = new ArrayList<>();
+public class Test implements Serializable {
 
-    public static void main(String[] args) {
-        fill();
-        System.out.println(calendar);
+    private static final long serialVersionUID = -1764970284520387975L;
+    public String name;
+
+    public Test() {
     }
 
-    private static void fill(){
-        LocalDate yearBefore = LocalDate.ofYearDay(LocalDate.now().getYear(), 1);
-        LocalDate limitDay = LocalDate.of(LocalDate.now().getYear() + 2, 1,1);
-        LocalDate dayToAdd = yearBefore;
-        while(dayToAdd.isBefore(limitDay)){
-            calendar.add(dayToAdd);
-            dayToAdd = dayToAdd.plusDays(1);
-        }
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                '}';
     }
-
 }
+
+
